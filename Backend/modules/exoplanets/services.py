4b13cb2c7c18611c35ad68f82069239b4d5ccea7
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-12-10 21:01:51 -0500

    Conflicts solved

--- Backend/modules/exoplanets/services.py
+++ Backend/modules/exoplanets/services.py
@@ -1,7 +1,6 @@
 import astropy.table
 from fastapi import HTTPException
 from .models import Exoplanet, RequestExoplanets
-from .utils import parse_dec_to_degrees, parse_ra_to_degrees
 from pydantic import BaseModel
 from astropy.table import Table
 import pyvo as vo
@@ -10,6 +9,7 @@ import requests
 import xml.etree.ElementTree as ET
 import json
 import re
+from typing import Any
 
 
 client = vo.dal.TAPService("https://exoplanetarchive.ipac.caltech.edu/TAP")
@@ -31,93 +31,10 @@ def result_to_exoplanet_list(result: astropy.table) -> list[Exoplanet]:
 
 
 async def find_some_exoplanets(index: int, amount: int)->tuple[bool, str]:
-
-
-    # URL base
-    url = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/IceTable/nph-iceTbl"
-
-    # Parámetros de la solicitud (ajusta según sea necesario)
-    params = {
-        "log": "TblView.ExoplanetArchive",
-        "workspace": "2024.12.02_10.04.18_002933/TblView/2024.12.05_16.11.34_009010",
-        "table": "/exodata/kvmexoweb/ExoTables/PS.tbl",
-        "pltxaxis": "",
-        "pltyaxis": "",
-        "checkbox": "1",
-        "initialcheckedval": "1",
-        "splitlabel": "0",
-        "wsoverride": "1",
-        "rowLabel": "rowlabel",
-        "connector": "true",
-        "dhx_no_header": "1",
-        "posStart": index,  # Inicio de las filas
-        "count": amount,    # Cantidad de filas a devolver
-        "dhxr1733443898337": "1"
-    }
-
-    response = requests.get(url, params=params)
-
-    if response.status_code == 200:
-        root = ET.fromstring(response.text)
-
-        # Crear una lista para almacenar los datos
-        planets = []
-
-        # Extraer información de las filas (<row>)
-        for row in root.findall(".//row"):
-            planet_data = {}
-            cells = row.findall("cell")
-            if len(cells) > 2:  # Asegúrate de que haya suficientes datos
-                planet_data["id"] = row.attrib.get("id")
-                #print("------------->", cells[2].text)
-                planet_data["name"] = re.findall(r">([^<]+)<",cells[2].text)[0]
-                planet_data["host_star"] = cells[3].text
-                # Estrellas en el sistema
-                planet_data["stars_amount"] = cells[5].text
-                # Año de descubrimientp
-                planet_data["discovery_year"] = cells[8].text
-                tmp =  re.findall(r">([^<]+)<", cells[15].text)
-                # Radio del planeta compoarado con la Tierra
-                if (len(tmp)>0):
-                    planet_data["radius"] = tmp[0]
-                else: 
-                    tmp = re.findall(r"\d+\.\d+(?=&)",cells[15].text)
-                    if len(tmp)>0: 
-                        planet_data["radius"] = tmp[0]
-                    else:
-                        planet_data["radius"] = ""
-                # Ascención recta: Posición en el cielo, en formato sexagesimal(grados, minutos, segundos)
-                planet_data["ra"] = parse_ra_to_degrees(cells[33].text)
-                # Declinación, similar a la latitud,  en formato sexagesimal(grados, minutos, segundos)
-                planet_data["dec"] = parse_dec_to_degrees(cells[34].text)
-                tmp =  re.findall(r">([^<]+)<", cells[35].text)
-                # Distancia en parsecs
-                if (len(tmp) > 0):
-                    planet_data["dist"] = tmp[0]
-                else: 
-                    tmp = re.findall(r"\d+\.\d+(?=&)",cells[35].text)
-                    if len(tmp)>0: 
-                        planet_data["dist"] = tmp[0]
-                    else:
-                        planet_data["dist"] = ""
-            planets.append(planet_data)
-
-        # Convertir a JSON
-        json_data = json.dumps(planets)
-        return True, json_data
-    else:
-        print(f"Error: {response.status_code}")
-        return False, ""
-
-
-
-
-async def find_exoplanets_by_name(name: str) -> str:
     global client
-    name = name.replace(' ','%')
-    query = f"SELECT pl_name, ra, dec, parallax FROM ps WHERE pl_name LIKE '%{name}%' LIMIT 20"
+
     query = f"""
-    SELECT DISTINCT
+    SELECT DISTINCT TOP {index + amount} 
         pl_name AS "name",
         hostname AS "host_star",
         sy_snum AS "stars_amount",
@@ -125,24 +42,76 @@ async def find_exoplanets_by_name(name: str) -> str:
         pl_rade AS "radius",
         rastr AS "ra",
         decstr AS "dec",
-        sy_dist AS "dist"
+        sy_dist AS "dist",
+        gaia_id
     FROM
-        ps
-    WHERE pl_name LIKE '%{name}%'
+        pscomppars
+    WHERE 
+        pl_name IS NOT NULL AND 
+        hostname IS NOT NULL AND 
+        sy_snum IS NOT NULL AND 
+        disc_year IS NOT NULL AND 
+        pl_rade IS NOT NULL AND 
+        rastr IS NOT NULL AND 
+        decstr IS NOT NULL AND 
+        sy_dist IS NOT NULL AND 
+        gaia_id IS NOT NULL
+    ORDER BY pl_name ASC
     """
     result = client.search(query)
     print(result)
-    planet_set = set()
-    if len(result) == 0: return ""
+    if len(result) == 0: return False,""
+    planets=[]
+    for i in range(index, min(index+amount, len(result))):
+        p: dict[Any, Any] = {}
+        #print(result[i])
+        for key in result[i]:
+            val = str(result[i][key])
+            p[key]= val 
+        planets.append(p)
+    #print(json.dumps(planets, indent=4))
+    return True, json.dumps(planets)
+
+
+
+
+async def find_exoplanets_by_name(name: str) -> str:
+    global client
+    name = name.replace(' ','%')
+    query = f"""
+    SELECT DISTINCT  
+        pl_name AS "name",
+        hostname AS "host_star",
+        sy_snum AS "stars_amount",
+        disc_year AS "discovery_year",
+        pl_rade AS "radius",
+        rastr AS "ra",
+        decstr AS "dec",
+        sy_dist AS "dist",
+        gaia_id
+    FROM
+        pscomppars
+    WHERE 
+        LOWER( pl_name ) LIKE LOWER( '%{name}%' ) AND 
+        hostname IS NOT NULL AND 
+        sy_snum IS NOT NULL AND 
+        disc_year IS NOT NULL AND 
+        pl_rade IS NOT NULL AND 
+        rastr IS NOT NULL AND 
+        decstr IS NOT NULL AND 
+        sy_dist IS NOT NULL AND 
+        gaia_id IS NOT NULL
+    """
+    result = client.search(query)
+    #print(result)
+    if len(result) == 0: return "";
     planets = []
     for row in result:
         p = {}
-        if row['name'] in planet_set: continue
         for key in row:
             val = str( row[key] )
             p[key] = val if val != 'nan' else ""
         planets.append(p)
-        planet_set.add(row['name'])
     return json.dumps(planets)
 
 
